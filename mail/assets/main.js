7c5711e3e9733942523877c05fa47193741d2bad
Nicola R <nicola.rizzi89@hotmail.it>
2020-11-17 16:07:45 +0100

    Mail
    Modified the Javascript code.

--- mail/assets/main.js
+++ mail/assets/main.js
@@ -3,9 +3,19 @@
 var mailList = ["pincopallo@gmail.com","ginogino@gmail.com"];
 var userMail = prompt("Insert your Email");
 
-if (userMail === mailList[0] || userMail === mailList[1] ){
-    console.log("You can enter the website");
-}else{
-    console.log("You need to register to our website");
-}
+var elMail = userMail;
+// console.log(userMail);
 
+// if (userMail === mailList[0] || userMail === mailList[1] ){
+//     console.log("You can enter the website");
+// }else{
+//     console.log("You need to register to our website");
+// }
+
+for(var i =0; i <= mailList.length; i++ ){
+    if(mailList[i] == elMail ){
+        console.log("You can Enter")
+    }else{
+        console.log("You need to Sign Up")
+    }
+}
